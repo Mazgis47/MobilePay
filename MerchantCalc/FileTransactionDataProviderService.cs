c294a30e90751f25d710ee58ffb6eb557dc82c94
Mindaugas Pranckevicius <mindaugas.pranckevicius@gmail.com>
2019-06-03 21:07:26 +0300

    Add renamed files

---
+++ MerchantCalc/FileTransactionDataProviderService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilePay.MerchantCalc
+{
+    /// <summary>
+    /// Reads transactions from file
+    /// </summary>
+    public class FileTransactionDataProviderService : DataProviderService, ITransactionDataProviderService
+    {
+        private string _transactionsFileName;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="transactionsFileName">Takes file name to read transactions from</param>
+        public FileTransactionDataProviderService(string transactionsFileName)
+        {
+            _transactionsFileName = transactionsFileName;
+        }
+
+        /// <summary>
+        /// Yields transactions that a read from file
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Transaction> GetTransactions()
+        {
+            using (StreamReader sr = new StreamReader(_transactionsFileName))
+            {
+                // Read the file string by string, and send transaction back as required.
+                while (sr.Peek() >= 0)
+                {
+                    String line = sr.ReadLine();
+                    //throw new NotImplementedException();
+                    yield return GetTransaction(line);
+                }
+            }
+        }
+
+    }
+}
