fb666d0e4f78abc70ca1da3ae5f17de2dcd8b7cd
Pranckevicius, Mindaugas: IT (LDN) <Mindaugas.Pranckevicius@barclayscorp.com>
2019-06-04 11:13:02 +0300

    Add TransactionFeeCalculator unitest

---
+++ UnitTestMerchantCalc/UnitTestTransactionFeeCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MobilePay.MerchantCalc;
+
+namespace UnitTestMerchantCalc
+{
+    [TestClass]
+    public class UnitTestTransactionFeeCalculator
+    {
+        [TestMethod]
+        public void ShouldCalculateByDefaultFee()
+        {
+            var basicTransactionFeeCalculator = new BasicTransactionFeeCalculator();
+            Assert.AreEqual(basicTransactionFeeCalculator.GetTransactionFee(100), 1);
+        }
+    }
+}
