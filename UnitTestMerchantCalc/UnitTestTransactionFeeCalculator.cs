6001f4a4f7843597c896cf1f5da4735c7eafa72a
Pranckevicius, Mindaugas: IT (LDN) <Mindaugas.Pranckevicius@barclayscorp.com>
2019-06-04 12:27:43 +0300

    Add tests

--- UnitTestMerchantCalc/UnitTestTransactionFeeCalculator.cs
+++ UnitTestMerchantCalc/UnitTestTransactionFeeCalculator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MobilePay.MerchantCalc;
 
@@ -13,11 +14,38 @@ namespace UnitTestMerchantCalc
             var basicTransactionFeeCalculator = new BasicTransactionFeeCalculator();
             Assert.AreEqual(basicTransactionFeeCalculator.GetTransactionFee("Any", 100), 1);
         }
+
         [TestMethod]
         public void ShouldCalculateByProvidedFee()
         {
             var basicTransactionFeeCalculator = new BasicTransactionFeeCalculator(2);
             Assert.AreEqual(basicTransactionFeeCalculator.GetTransactionFee("Any", 100), 2);
+        }
+
+        [TestMethod]
+        public void ShouldCalculateDiscountForProvidedFee()
+        {
+            var discountTransactionFeeCalculator = new DiscountTransactionFeeCalculator(new BasicTransactionFeeCalculator(1)
+                            , new Dictionary<string, double>() { { "DISCOUNTMERCHANT", 10 } });
+            Assert.AreEqual(discountTransactionFeeCalculator.GetTransactionFee("DISCOUNTMERCHANT", 120), 1.08);
+        }
+
+        [TestMethod]
+        public void ShouldNotCalculateDiscountForProvidedFee()
+        {
+            var discountTransactionFeeCalculator = new DiscountTransactionFeeCalculator(new BasicTransactionFeeCalculator(1)
+                            , new Dictionary<string, double>() { { "DISCOUNTMERCHANT", 10 } });
+            Assert.AreEqual(discountTransactionFeeCalculator.GetTransactionFee("ANY_OTHER", 120), 1.20);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException),
+            "Please provide valid baseTransactionFeeCalculator")]
+        public void ShouldThrowError()
+        {
+            var discountTransactionFeeCalculator = new DiscountTransactionFeeCalculator(null
+                            , new Dictionary<string, double>() { { "DISCOUNTMERCHANT", 10 } });
+            discountTransactionFeeCalculator.GetTransactionFee("ANY_OTHER", 120);
         }
     }
 }
